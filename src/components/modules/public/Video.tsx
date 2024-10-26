b324406177c89dba8a3526f4e8b69d636794f174
Haseeb <haseeb.salman1234@outlook.com>
2024-10-26 07:00:25 +0500

    image changes

--- src/components/modules/public/Video.tsx
+++ src/components/modules/public/Video.tsx
@@ -2,7 +2,26 @@
 
 export default function Video({url,imgUrl,extraClass}:{url:string,imgUrl:string,extraClass?:string}){
     return (
-        <video controls className={`w-full max-w-screen-lg h-auto  absolute top-20 lg:top-24 ${extraClass}`}  width="320" height="240"  poster={imgUrl}>
+        <video onTouchStart={(e:any)=>{
+          console.log(e.target.paused)
+          if(e.target.paused){
+            e.target.play()
+            return
+          }
+          e.target.pause()
+
+          }} onClick={(e:any)=>{
+          console.log(e.target.paused)
+          if(e.target.paused){
+            e.target.play()
+            return
+          }
+          e.target.pause()
+
+          }} onEnded={(e:any)=>{
+            e.target.currentTime=0
+            console.log('ended')
+          }} className={`w-full max-w-screen-lg h-auto  absolute top-20 lg:top-24 ${extraClass}`}  width="320" height="240"  poster={imgUrl}>
           <source src={url} type="video/mp4"/>
           {/* <source src="movie.ogg" type="video/ogg"> */}
         Your browser does not support the video tag.
